63fc3328474f552f874f706269c43ab570cacdc9
nicoson <nicoson@163.com>
2015-02-27 14:53:50 -0500

    add new effects in navbar

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,4 +3,24 @@ angular.module("movieMgr", [])
 	$http.get("movies.json").success(function(data){
 		$scope.films = data;
 	});
-}]);
+	$scope.navCtr = true;
+}])
+
+.directive("myDir", function(){
+	return{
+		restrict: "AE",
+		link: function(scope, ele, attr){
+			var searchBarHeight = ele.offset().top;
+			$(window).on("scroll",function(){				
+				console.log($(this).scrollTop()+":"+searchBarHeight+":"+scope.navCtr);
+				if($(this).scrollTop() > searchBarHeight){
+					scope.navCtr = false;
+				}else{
+					scope.navCtr = true;
+				}
+
+				scope.$apply();
+			});
+		}
+	}
+});
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Movie Info System" });
-  res.set('Access-Control-Allow-Origin', '*');
+  //res.set('Access-Control-Allow-Origin', '*');
   //res.json(film);
 });
 
